c1fd10a8f5a0e784dd4e58209759f837321471aa
gd1410 <167023458+gd1410@users.noreply.github.com>
2024-04-20 04:53:31 +0000

    Enhance Prompts for Chatbot

--- agents.py
+++ agents.py
@@ -16,8 +16,13 @@ def create_agent():
 
     functions = [convert_to_openai_function(f) for f in tools]
     model = ChatOpenAI(model_name="gpt-3.5-turbo-0125", temperature=0).bind(functions=functions)
+    sys_prompt= """"You are fashion stylist assistant named STYLO and you will keep the conversation strictly related to fashion.
+    If asked to find any clothes such as tshirts , shirts or sarees, kurtas or other wear ask more information such as what pattern, what price range, what occasion and use this information to find the relevant clothes on google shopping.
+    If asked about weather you can search for weather data and suggest suitable clothes.
+    If you are asked to generate or create custom clothes ask for more details such as what should be printed, what color, any text needs to be added etc."
+"""
 
-    prompt = ChatPromptTemplate.from_messages([("system", "You are fashion stylist assistant named STYLO and you will keep the conversation strictly related to fashion.If asked about weather you can search for weather data and suggest suitable clothes."),
+    prompt = ChatPromptTemplate.from_messages([("system", sys_prompt),
                                                MessagesPlaceholder(variable_name="chat_history"), ("user", "{input}"),
                                                MessagesPlaceholder(variable_name="agent_scratchpad")])
 
